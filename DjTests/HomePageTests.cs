31efdb6925dc29f5cca115105b4ae8bf36a2855b
TimotejEkart <timotejekart12345@gmail.com>
2025-03-11 17:43:58 +0100

    unit test

---
+++ DjTests/HomePageTests.cs
@@ -0,0 +1,53 @@
+﻿using Bunit;
+using Dj.Components.Pages;
+using Xunit;
+using Dj; // Importirajte svoj Namespace za komponento Home.razor
+
+public class HomePageTests : TestContext
+{
+    [Fact]
+    public void HomePage_RendersCorrectly()
+    {
+        var component = RenderComponent<Home>();
+
+        var h1Element = component.Find("h1.display-1");
+
+        Assert.Equal("DJ Asistent", h1Element.TextContent.Trim());
+    }
+
+    [Fact]
+    public void HomePage_ContainsMoreAboutUsButton()
+    {
+        var component = RenderComponent<Home>();
+
+        var button = component.Find("a.btn.btn-primary");
+        Assert.Equal("Več o nas", button.InnerHtml);
+    }
+
+    [Fact]
+    public void HomePage_ContainsFunctionalitiesButton()
+    {
+        var component = RenderComponent<Home>();
+
+        var button = component.Find("a.btn.btn-secondary");
+        Assert.Equal("Funkcionalnosti", button.InnerHtml);
+    }
+
+    [Fact]
+    public void HomePage_DisplaysFeaturesSection()
+    {
+        var component = RenderComponent<Home>();
+
+        var featuresSection = component.Find("#features");
+        Assert.NotNull(featuresSection);
+    }
+
+    [Fact]
+    public void HomePage_ShowsAboutSection()
+    {
+        var component = RenderComponent<Home>();
+
+        var aboutSection = component.Find("#about");
+        Assert.NotNull(aboutSection);
+    }
+}
